0a5cfcf6996021fe702a3edfd7c8764453d80ef0
kiriakospapa <kyriakospapadopoulosauto@gmail.com>
2022-02-11 16:40:49 +0200

    completed plot_artists_with_most_songs function

--- functions.py
+++ functions.py
@@ -1,7 +1,19 @@
+from unicodedata import name
 import matplotlib.pyplot as plt
+import numpy
+import plotly.express as px
 
+
+def make_autopct(values):
+    """Returns the values as labels for a pie char and not the percentages"""
+    def my_autopct(pct):
+        total = sum(values)
+        val = int(round(pct*total/100.0))
+        return f'{val} songs'
+    return my_autopct
 
 def plot_artists_with_most_songs(singers, n=10):
+    """Plots the top N singers with the most liked songs"""
     singers_unique = set(singers)
     names = []
     times = []
@@ -14,9 +26,10 @@ def plot_artists_with_most_songs(singers, n=10):
 
     times = sorted(times)
 
-    plt.pie(times[-n:], labels=singers_unique[-n:], shadow=True)
+    plt.pie(times[-n:], labels=singers_unique[-n:], autopct=make_autopct(times[-n:]), shadow=True)
+    plt.title(label=f"Top {n} Artists With Most Liked Songs")
     plt.show() 
- 
+    
 
 if __name__ =='__main__':
     pass
